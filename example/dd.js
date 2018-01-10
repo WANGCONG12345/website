2758142e0a977bbc543d0c0a52f03f2037d098f5
wangcong <Administrator@MS-20160809TEGJ>
2018-01-10 10:19:24 +0800

    网站

---
+++ example/dd.js
@@ -0,0 +1,26 @@
+ <div id="Fdemos1">
+                 <table border="0" cellspacing="0" cellpadding="0">
+                  <tbody>
+                  <tr>
+               <td>
+               <ul style="width:100%; float:left; overflow:hidden">
+                   <li style="width:292px; background:#069f0a; border:1px solid #069f0a; text-align:center; float:left; height:260px; margin:5px; overflow:hidden">
+                 <a href="#">
+                 <img src="./images/yst_1.jpg" width="292" height="220" alt="泰州鹅">
+                 <div style="line-height:25px; font-size:14px; color:#fff;">泰州鹅</div>
+                 </a>
+                  </li>
+                <li style="width:292px; background:#069f0a; border:1px solid #069f0a; text-align:center; float:left; height:260px; margin:5px; overflow:hidden">
+               <a href="/index.php?c=content&amp;a=show&amp;id=965">
+               <img src="./images/yst_2.jpg" width="292" height="220" alt="大三花杂交鹅">
+                <div style="line-height:25px; font-size:14px; color:#fff;">大三花杂交鹅</div>
+                </a>
+               </li> 
+
+
+            </ul>
+            </td>
+           </tr>
+            </tbody>
+          </table>
+           </div> 
